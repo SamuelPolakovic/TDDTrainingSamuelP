bb7315d671dac559d7156b020cde63caa1be22d3
SamuelPolakovic <samuel.polakovic@siemens-healthineers.com>
2021-09-06 16:57:19 +0200

    Simple Fraction additions added, if the addition equals to integer number

---
+++ TDDTraining/TDDTraining/Fraction.cs
@@ -0,0 +1,70 @@
+﻿/* -------------------------------------------------------------------------------------------------
+   Restricted - Copyright (C) Siemens Healthcare GmbH, Inc., 2021. All rights reserved
+   ------------------------------------------------------------------------------------------------- */
+   
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+using Microsoft.VisualBasic;
+
+namespace TDDTraining
+{
+    public class Fraction
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+            ChangeToLowestPossibleDenominator();
+        }
+
+        private void ChangeToLowestPossibleDenominator()
+        {
+            if (Numerator == 1 || Numerator == 0)
+            {
+                return;
+            }
+            if (Denominator % Numerator == 0)
+            {
+                var divider = Numerator;
+                Denominator = Denominator / divider;
+                Numerator = Numerator / divider;
+            }
+            else if(Numerator % Denominator == 0)
+            {
+                Numerator = Numerator / Denominator;
+                Denominator = 1;
+            }
+        }
+
+        public Fraction(int numerator)
+        {
+            Numerator = numerator;
+            Denominator = 1;
+        }
+
+        public double Value { get; set; }
+
+        public static Fraction Add(Fraction fractionOne, Fraction fractionTwo)
+        {
+            if (fractionOne.Denominator == fractionTwo.Denominator)
+            {
+                return new Fraction(fractionOne.Numerator + fractionTwo.Numerator,fractionOne.Denominator);
+            }
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            if (Denominator == 1)
+            {
+                return Numerator.ToString();
+            }
+            return base.ToString();
+        }
+
+    }
+}
---
+++ TDDTraining/TDDTrainingTests/FractionTests.cs
@@ -0,0 +1,79 @@
+﻿/* -------------------------------------------------------------------------------------------------
+   Restricted - Copyright (C) Siemens Healthcare GmbH, Inc., 2021. All rights reserved
+   ------------------------------------------------------------------------------------------------- */
+   
+using System;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using NUnit.Framework;
+using TDDTraining;
+
+namespace TDDTrainingTests
+{
+    public class FractionTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 0)]
+        [TestCase(0, 1)]
+        [TestCase(1, 1)]
+        [TestCase(5, 3)]
+        public void FractionAddSimpleNumbersTest(int firstNumber, int secondNumber)
+        {
+            var fractionOne = new Fraction(firstNumber);
+            var fractionTwo = new Fraction(secondNumber);
+
+            var expectedResult = new Fraction(firstNumber+ secondNumber);
+
+            var sut = Fraction.Add(fractionOne, fractionTwo);
+
+            Assert.AreEqual(expectedResult.ToString(), sut.ToString());
+        }
+
+        [TestCase(0)]
+        [TestCase(156)]
+        [TestCase(-15)]
+        public void FractionConstructorSimpleNumbersTest(int firstNumber)
+        {
+            var sut = new Fraction(firstNumber);
+
+            var expectedResult = firstNumber.ToString();
+
+            Assert.AreEqual(expectedResult, sut.ToString());
+        }
+
+        [TestCase(2, 4)]
+        [TestCase(6, 12)]
+        public void FractionRepresentationTest(int firstNumber, int secondNumber)
+        {
+            var sut = new Fraction(firstNumber, secondNumber);
+
+            var expectedResult = new Fraction(1,2);
+
+            Assert.AreEqual(expectedResult.ToString(), sut.ToString());
+        }
+
+        [TestCase(12, 6)]
+        public void FractionRepresentationDenominatorIsLowerTest(int firstNumber, int secondNumber)
+        {
+            var sut = new Fraction(firstNumber, secondNumber);
+
+            var expectedResult = "2";
+
+            Assert.AreEqual(expectedResult, sut.ToString());
+        }
+
+        [Test]
+        public void FractionAddSameDenominatorTest()
+        {
+            var fractionOne = new Fraction(1,2);
+            var fractionTwo = new Fraction(1,2);
+
+            var expectedResult = new Fraction(1);
+
+            var sut = Fraction.Add(fractionOne, fractionTwo);
+
+            Assert.AreEqual(expectedResult.ToString(), sut.ToString());
+        }
+    }
+}
